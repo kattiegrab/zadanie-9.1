5bb2c44a4faf8b2bab148c777143bc94d9d4c3d1
Kasia Grabowska <kattie.grab@gmail.com>
2017-04-26 19:46:26 +0200

    zad-kod

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 //Tworzenie funkcji
 var text = "Nieprawidłowe dane";
-var getTriangleArea = getTriangleArea(a, h);
+
 
 function getTriangleArea(a, h) {
-	if(a > 0, h > 0) {
+	if(a > 0 || h > 0) {
 		return a*h/2		
 	}
-	else if (a < 0, h < 0) {
+	else if (a < 0 || h < 0) {
 		return text;
 	}	
 }
 
-getTriangleArea(a, h);
+//getTriangleArea(a, h);
 
 console.log(getTriangleArea(10, 6))
 
@@ -19,3 +19,11 @@ console.log(getTriangleArea(10, 6))
 var triangle1Area = getTriangleArea(10 ,15);
 var triangle2Area = getTriangleArea(2, 5);
 var triangle3Area = getTriangleArea(20, 60);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
+
+var triangle4Area = getTriangleArea(-1, -5);
+
+console.log(triangle4Area);
