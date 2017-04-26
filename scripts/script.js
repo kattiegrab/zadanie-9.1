428ecf7d3a62e6008a2eddce405b8320be404f92
Kasia Grabowska <kattie.grab@gmail.com>
2017-04-26 15:10:24 +0200

    kodilla-zadanie9.1

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+//Tworzenie funkcji
+var text = "Nieprawidłowe dane";
+var getTriangleArea = getTriangleArea(a, h);
+
+function getTriangleArea(a, h) {
+	if(a > 0, h > 0) {
+		return a*h/2		
+	}
+	else if (a < 0, h < 0) {
+		return text;
+	}	
+}
+
+getTriangleArea(a, h);
+
+console.log(getTriangleArea(10, 6))
+
+// Zmienne posiadające pole trójkąta
+var triangle1Area = getTriangleArea(10 ,15);
+var triangle2Area = getTriangleArea(2, 5);
+var triangle3Area = getTriangleArea(20, 60);
